17f81a290ea5d367efcc01d187fd3927f77c0242
Blightning97 <jasonzatre@hotmail.com>
2021-09-22 22:46:42 -0600

    Task 18 - Added a way to show the new command now_playing and it displays the song name, duration and thumbnail -For the duration another method was created to give the amount in hours, minutes and seconds.

--- music/music_commands.py
+++ music/music_commands.py
@@ -9,3 +9,6 @@ SKIP_COMMAND_ALIASES = ["saltela","s"]
 
 ############################### Shuffle Command
 SHUFFLE_COMMAND_ALIASES = ["barajela","mezclela", "b", "m"]
+
+############################### Now Playing Command
+NOW_PLAYING_COMMAND_ALIASES = ["np","cual", "cual suena"]
