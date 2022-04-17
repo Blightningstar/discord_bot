194d97e6d45f1cded81fb015a0487a76ed9f593e
Blightning97 <jasonzatre@hotmail.com>
2022-04-17 01:50:28 -0600

    Task 58 - Adding Static Files - Adding ngrok - Toying with the sidebar

--- discord_bot/music_bot/urls.py
+++ discord_bot/music_bot/urls.py
@@ -1,7 +1,8 @@
-from .views import music_commands_views, play_view
+from .views import music_commands_views, play_view, now_playing_view
 from django.urls import path
 
 urlpatterns = [
     path("commands_help/", music_commands_views, name="commands-help"),
-    path("commands_help/play", play_view, name="commands-play")
+    path("commands_help/play", play_view, name="commands-play"),
+    path("commands_help/now_playing", now_playing_view, name="commands-now-playing")
 ]
