bd8cad1d4db6246bff8a5df31b0ac7528a507192
Blightning97 <jasonzatre@hotmail.com>
2022-04-17 14:09:40 -0600

    Task 58 - Added move command view, url and html

--- discord_bot/music_bot/urls.py
+++ discord_bot/music_bot/urls.py
@@ -1,9 +1,10 @@
-from .views import music_commands_views, play_view, now_playing_view
+from .views import music_commands_views, play_view, now_playing_view, move_view
 from django.urls import path
 
 urlpatterns = [
     path("", music_commands_views, name="commands-help"),
     path("commands_help/", music_commands_views, name="commands-help"),
     path("commands_help/play", play_view, name="commands-play"),
-    path("commands_help/now_playing", now_playing_view, name="commands-now-playing")
+    path("commands_help/now_playing", now_playing_view, name="commands-now-playing"),
+    path("commands_help/move", move_view, name="commands-move")
 ]
--- discord_bot/music_bot/views.py
+++ discord_bot/music_bot/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .music_commands import PLAY_COMMAND_ALIASES, NOW_PLAYING_COMMAND_ALIASES
+from .music_commands import PLAY_COMMAND_ALIASES, NOW_PLAYING_COMMAND_ALIASES, MOVE_COMMAND_ALIASES
 
 def music_commands_views(request):
     return render(request, "music_bot/music_commands_help.html")
@@ -11,3 +11,7 @@ def play_view(request):
 def now_playing_view(request):
     context = {"command_aliases": NOW_PLAYING_COMMAND_ALIASES, "base_command":"now_playing"}
     return render(request, "music_bot/now_playing.html", context=context)
+
+def move_view(request):
+    context = {"command_aliases": MOVE_COMMAND_ALIASES, "base_command":"move"}
+    return render(request, "music_bot/move.html", context=context)
