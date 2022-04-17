2470779a0fc6a541e557b797c11520209f663a24
Blightning97 <jasonzatre@hotmail.com>
2022-04-17 13:01:14 -0600

    Task 58 - Collapsable Sidebar working, tested on PC and Phone - Added redirection of main view to help_commands view - modifications in text of commands htmls

--- discord_bot/music_bot/urls.py
+++ discord_bot/music_bot/urls.py
@@ -2,6 +2,7 @@ from .views import music_commands_views, play_view, now_playing_view
 from django.urls import path
 
 urlpatterns = [
+    path("", music_commands_views, name="commands-help"),
     path("commands_help/", music_commands_views, name="commands-help"),
     path("commands_help/play", play_view, name="commands-play"),
     path("commands_help/now_playing", now_playing_view, name="commands-now-playing")
