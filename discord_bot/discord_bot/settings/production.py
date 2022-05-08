f445fcfdbde79e22eda5f33f8df836d4045dfbc1
Blightning97 <jasonzatre@hotmail.com>
2022-05-07 23:35:36 -0600

    Task 58 - production.py changes

--- discord_bot/discord_bot/settings/production.py
+++ discord_bot/discord_bot/settings/production.py
@@ -1,12 +1,7 @@
 from .base import *
 
-test_mode = os.getenv("TEST_MODE")
+DEBUG = False
 
-if test_mode=="True":
-    DEBUG = True
-    ALLOWED_HOSTS = [".herokuapp.com", "localhost", "0.0.0.0"]
-else: 
-    DEBUG = False
-    ALLOWED_HOSTS = [".herokuapp.com"]
+ALLOWED_HOSTS = [".herokuapp.com"]
 
 BOT_NAME = "Marbot"
