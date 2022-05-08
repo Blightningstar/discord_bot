b440f9f27e3a173e4abd0c542c202258b881e9b9
Blightning97 <jasonzatre@hotmail.com>
2022-05-07 22:37:11 -0600

    Task 58 - Changing dev and production settings

--- discord_bot/discord_bot/settings/dev.py
+++ discord_bot/discord_bot/settings/dev.py
@@ -7,8 +7,3 @@ ALLOWED_HOSTS = ["localhost","0.0.0.0",".ngrok.io"]
 BOT_NAME = "Marbotest"
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-# HTTPS settings
-SESSION_COOKIE_SECURE = False
-CSRF_COOKIE_SECURE = False
-SECURE_SSL_REDIRECT = False
--- discord_bot/discord_bot/settings/production.py
+++ discord_bot/discord_bot/settings/production.py
@@ -1,13 +1,12 @@
 from .base import *
 
 test_mode = os.getenv("TEST_MODE")
-DEBUG = True if test_mode=="True" else False
 
-ALLOWED_HOSTS = [".herokuapp.com"]
+if test_mode=="True":
+    DEBUG = True
+    ALLOWED_HOSTS = [".herokuapp.com", "localhost", "0.0.0.0"]
+else: 
+    DEBUG = False
+    ALLOWED_HOSTS = [".herokuapp.com"]
 
 BOT_NAME = "Marbot"
-
-# HTTPS settings
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
-SECURE_SSL_REDIRECT = True
