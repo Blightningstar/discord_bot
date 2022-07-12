4cc4e5755a07d3e6672bf354c4e9fea84ecaf3ef
Blightning97 <jasonzatre@hotmail.com>
2022-07-11 22:44:36 -0600

    Adding better max_length to thumbnail

--- discord_bot/music_bot/models.py
+++ discord_bot/music_bot/models.py
@@ -4,7 +4,7 @@ class SongLog(models.Model):
     url = models.URLField(primary_key=True)
     title = models.CharField(max_length=1000)
     duration = models.FloatField()
-    thumbnail = models.ImageField()
+    thumbnail = models.ImageField(max_length=225)
 
     def __str__(self):
         return self.title
