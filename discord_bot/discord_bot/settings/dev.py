602b8ff4be074326012f111619b4c49a359bd930
Blightning97 <jasonzatre@hotmail.com>
2022-12-27 21:49:45 -0600

    Task 115 - Adding new psql database using docker with wsl2

--- discord_bot/discord_bot/settings/dev.py
+++ discord_bot/discord_bot/settings/dev.py
@@ -10,11 +10,11 @@ MUSIC_CHANNEL = 894034318089920542
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "dnqo7edhna8sk",
-        "USER": "jpfwcyherkxjak",
-        "PASSWORD": "7357b601741e4e797d5d27e97c793ecf89ce1b324da00735f26e1200613f1bf4",
-        "HOST": "ec2-44-205-41-76.compute-1.amazonaws.com",
-        "PORT": "5432",
+        "NAME": "discordBot",
+        "USER": "postgres",
+        "PASSWORD": "postgrespw",
+        "HOST": "localhost",
+        "PORT": "49155",
     }
 }
 
